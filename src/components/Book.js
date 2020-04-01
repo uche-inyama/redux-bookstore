c94332272c84826b31daa5044e9431757593da78
uche-inyama <depoulet@yahoo.com>
2020-04-01 11:05:50 +0100

    adjust category be above the title of the book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Book.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Book.css";
 
-const Book = ({
-  title, category, handleRemoveBook, id,
-}) => (
+const Book = ({ title, category, handleRemoveBook, id }) => (
   <>
     <tr className="Book">
-      <td className="bookTitle">{title}</td>
       <td className="bookCategory">{category}</td>
+      <td className="bookTitle">{title}</td>
       <td>
         <button
           type="button"
@@ -26,6 +24,6 @@ Book.propTypes = {
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
-  handleRemoveBook: PropTypes.func.isRequired,
+  handleRemoveBook: PropTypes.func.isRequired
 };
 export default Book;
